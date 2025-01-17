5b21e71a9ab414d6e785bc9a2b7b5e6ed08eb77e
Hari <157208780+SreehariAjesh@users.noreply.github.com>
2025-01-17 18:41:13 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -113,3 +113,24 @@ self.addEventListener('push', function(event) {
   };
   self.registration.showNotification(data.title, notificationOptions);
 });
+// Firebase Messaging Service Worker
+
+self.addEventListener("push", (event) => {
+
+    const notif = event.data.json().notification;
+
+    event.waitUntil(self.registration.showNotification(notif.title , {
+        body: notif.body,
+        icon: notif.image,
+        data: {
+            url: notif.click_action
+        }
+    }));
+
+});
+
+self.addEventListener("notificationclick", (event) => {
+
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+
+});
