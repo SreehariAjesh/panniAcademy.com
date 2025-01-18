1f022237f56752e5fe8839a7ad340005aa7068df
Hari <157208780+SreehariAjesh@users.noreply.github.com>
2025-01-18 21:05:29 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
+// Firebase Messaging Service Worker
+self.addEventListener("push", (event) => {
+    const notif = event.data.json().notification;
+
+    event.waitUntil(self.registration.showNotification(notif.title , {
+        body: notif.body,
+        icon: notif.image,
+        data: {
+            url: notif.click_action
+        }
+    }));
+});
+
+self.addEventListener("notificationclick", (event) => {
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+});
+
 const CACHE_NAME = 'panni-academy-cache-v1';
 const urlsToCache = [
   '/',
@@ -71,19 +88,4 @@ self.addEventListener('push', function(event) {
   self.registration.showNotification(data.title, notificationOptions);
 });
 
-// Firebase Messaging Service Worker
-self.addEventListener("push", (event) => {
-    const notif = event.data.json().notification;
 
-    event.waitUntil(self.registration.showNotification(notif.title , {
-        body: notif.body,
-        icon: notif.image,
-        data: {
-            url: notif.click_action
-        }
-    }));
-});
-
-self.addEventListener("notificationclick", (event) => {
-    event.waitUntil(clients.openWindow(event.notification.data.url));
-});
